e0d8a3ae0ccd84122ba6301df2d0bc3baf01e28d
Caryl Gros <caryl.gros@he-arc.ch>
2019-06-07 16:27:47 +0200

    Test commit 3 classe correcte et fonctionnelle

--- PcoursGenieLog/src/ch/hearc/coursgenielog/SuperPile.java
+++ PcoursGenieLog/src/ch/hearc/coursgenielog/SuperPile.java
@@ -17,5 +17,63 @@ public class SuperPile extends Stack<Integer>
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
+	public synchronized static SuperPile getInstance() // SIngleton
+		{
+		if (instance == null)
+			{
+			System.out.println("J'existe");
+			instance = new SuperPile();
+			}
+		return instance;
+		}
 
+	public void Empiler(int x)
+		{
+		push(x);
+		}
+
+	public int Depiler()
+		{
+		if (taille() == 0) // Observable ..
+			{
+			notifyElement();
+			}
+		return pop();
+		}
+
+	public int Sommet() // FACADE
+		{
+		int first = pop();
+		push(first);
+		return first;
+		}
+
+	public int SousSommet() // FACADE
+		{
+		int first = pop();
+		int second = pop();
+		push(second);
+		push(first);
+		return second;
+		}
+
+	public int taille()
+		{
+		return size();
+		}
+
+	/*------------------------------------------------------------------*\
+	|*							Methodes Private						*|
+	\*------------------------------------------------------------------*/
+
+	private void notifyElement()
+		{
+		System.out.println("LA PILE EST VIDE");
+
+		}
+	/*------------------------------------------------------------------*\
+	|*							Attributs Private						*|
+	\*------------------------------------------------------------------*/
+
+	private static SuperPile instance;
 	}
